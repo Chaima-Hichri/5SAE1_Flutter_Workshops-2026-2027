4fd22251cf99dcf2c5c8c04e2f747c1bb5205c2c
chaimahichri <chaima.hichri1@esprit.tn>
2026-09-23 10:30:14 +0100

    create card film item

---
+++ lib/FilmCardItem.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+class FilmCardItem extends StatelessWidget {
+  String title,imagepath;
+   FilmCardItem({super.key,required this.title,required this.imagepath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child:Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Image.asset("assets/images/"+imagepath),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                  Icon(Icons.star,color: Colors.amber,size: 25,)
+                ],
+              ),
+            )
+          ],
+        ),
+      ) ,
+    );
+  }
+}
--- lib/GStore.dart
+++ lib/GStore.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_workshops_5sae1_2627/FilmCardItem.dart';
 class Gstore extends StatefulWidget {
   const Gstore({super.key});
 
@@ -17,69 +18,8 @@ class _GstoreState extends State<Gstore> {
       body:SingleChildScrollView(
         child: Column(
           children: [
-            Card(
-              elevation: 6,
-              child:Padding(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  children: [
-                    Image.asset("assets/images/HouseOfDead.jpg"),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("House Of Dead",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                          Icon(Icons.star,color: Colors.amber,size: 25,)
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ) ,
-            ),
-            Card(
-              elevation: 6,
-              child:Padding(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  children: [
-                    Image.asset("assets/images/thegrudge.jpg"),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("The Grudge",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                          Icon(Icons.star,color: Colors.amber,size: 25,)
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ) ,
-            ),
-            Card(
-              elevation: 6,
-              child:Padding(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  children: [
-                    Image.asset("assets/images/iceroad.jpg"),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Ice Road",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                          Icon(Icons.star,color: Colors.amber,size: 25,)
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ) ,
-            )
+           FilmCardItem(title: "The Grudge", imagepath: "thegrudge.jpg"),
+           FilmCardItem(title: "IceRoad", imagepath: "iceroad.jpg"),
           ],
         ),
       ) ,
