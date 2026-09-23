44a7f1b5f6d48f01344e4d9404e925f12be045c9
chaimahichri <chaima.hichri1@esprit.tn>
2026-09-23 11:31:04 +0100

    create list view films

---
+++ lib/Film.dart
@@ -0,0 +1,5 @@
+class Film {
+  final String title,description,image;
+  final int price ;
+  const Film(this.title,this.image,this.description,this.price);
+}
--- lib/GStore.dart
+++ lib/GStore.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_workshops_5sae1_2627/FilmCardItem.dart';
+
+import 'Film.dart';
 class Gstore extends StatefulWidget {
   const Gstore({super.key});
 
@@ -8,6 +10,11 @@ class Gstore extends StatefulWidget {
 }
 
 class _GstoreState extends State<Gstore> {
+  final List<Film> films = const [
+    const Film("House Of Dead", "HouseOfDead.jpg","The House of the Dead is a classic arcade light gun shooter series from Sega that features government agents fighting hordes of biologically engineered undead and mutants",300),
+    const Film("IceRoad", "iceroad.jpg","The Ice Road follows a team of truck drivers on a dangerous mission over frozen lakes and winter roads to deliver a crucial component to save workers trapped in .",200),
+    const Film("The Grudge", "thegrudge.jpg","The Grudge is a curse, born when someone dies in extreme rage or sorrow and lingers where the person dies. Those who encounter it will die, and the curse is ..",150),
+  ];
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -15,14 +22,11 @@ class _GstoreState extends State<Gstore> {
         title: Text("G-STORE",style: TextStyle(color: Colors.white),),
         backgroundColor: Colors.black,
       ),
-      body:SingleChildScrollView(
-        child: Column(
-          children: [
-           FilmCardItem(title: "The Grudge", imagepath: "thegrudge.jpg"),
-           FilmCardItem(title: "IceRoad", imagepath: "iceroad.jpg"),
-          ],
-        ),
-      ) ,
+      body:ListView.builder(
+        itemCount: films.length,
+          itemBuilder: (context,index){
+          return FilmCardItem(title: films[index].title, imagepath: films[index].image);
+          })
     );
   }
 }
